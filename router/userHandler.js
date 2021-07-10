be201bfd421eb92f40aafc6d7327b68c6f32cc4d
marzuk(linux) <linux.marzukzarir@gmail.com>
2021-07-11 01:09:22 +0600

    feat: userHandler 'get' reqMethod handler created ref: validator file encapsulation removed. termsAgree is optional field.

--- router/userHandler.js
+++ router/userHandler.js
@@ -7,8 +7,9 @@
  */
 
 const { validatePostData } = require('../utils/validator')
-const { hash } = require('../utils/utils')
+const { hash, parsedJSON } = require('../utils/utils')
 const db = require('../lib/database')
+const { _isValidPhone: checkPhone } = require('../utils/validator')
 
 const handler = {}
 
@@ -27,9 +28,32 @@ handler.userHandler = (reqProperty, callback) => {
 
 // We declare new object with reqMethod wise func for encapsulation
 const user = {}
+
+// Handle get request
 user.get = (reqProperty, callback) => {
-    callback(200)
+    const phone = checkPhone(reqProperty.queryStrings.phone, 'string', 11)
+
+    // If provided queryString's phone field is valid, we read '.db/user/{reqBody}.json' file
+    if (phone) {
+        db.readData('user', phone, (err, data) => {
+            // Read data is json formate but we need to response object
+            const user = { ...parsedJSON(data) }
+            // We don't want to access 'password' field in get req. so, we delete pass field
+            delete user.password
+
+            // If err not happen we response user object
+            if (!err && user) {
+                callback(200, user)
+            } else {
+                callback(500, { status: "Couldn't read data" })
+            }
+        })
+    } else {
+        callback(404, { status: 'User not found' })
+    }
 }
+
+// Handle post request
 user.post = (reqProperty, callback) => {
     const isValid = validatePostData(reqProperty.body)
 
@@ -52,7 +76,11 @@ user.post = (reqProperty, callback) => {
         callback(400, { status: 'There is a problem in your request' })
     }
 }
+
+// Handle put request
 user.put = (reqProperty, callback) => {}
+
+// Handle delete request
 user.delete = (reqProperty, callback) => {}
 
 module.exports = handler
--- utils/validator.js
+++ utils/validator.js
@@ -9,14 +9,14 @@
 const validator = {}
 
 validator.validatePostData = ({ firstName, lastName, phone, password, termsAgree }) => {
-    firstName = utils._isValidName(firstName, 'string')
-    lastName = utils._isValidName(lastName, 'string')
-    phone = utils._isValidPhone(phone, 'string', 11)
-    password = utils._isValidPassword(password)
+    firstName = validator._isValidName(firstName, 'string')
+    lastName = validator._isValidName(lastName, 'string')
+    phone = validator._isValidPhone(phone, 'string', 11)
+    password = validator._isValidPassword(password)
     termsAgree = typeof termsAgree === 'boolean' ? termsAgree : false
 
     // If every fields return true we return validated object
-    if (firstName && lastName && phone && password && termsAgree) {
+    if (firstName && lastName && phone && password) {
         return { firstName, lastName, phone, password, termsAgree }
     }
 
@@ -28,10 +28,9 @@ validator.validatePostData = ({ firstName, lastName, phone, password, termsAgree
  * * function contains 'optional chaining' feature which is introduced in es11 in js
  * * bcz if a field is missing in object app is will crash. that's why we use optional chaining
  */
-const utils = {}
 
 // Check is name trimmed length is greater than 0 and less than 10 character
-utils._isValidName = (nameField, type) => {
+validator._isValidName = (nameField, type) => {
     nameField = nameField?.trim()
     if (typeof nameField === type && nameField?.length > 0 && nameField?.length <= 10) {
         return nameField
@@ -40,14 +39,14 @@ utils._isValidName = (nameField, type) => {
 }
 
 // Check is phone number trimmed length is greater than provided length
-utils._isValidPhone = (phoneField, type, phoneLength) => {
+validator._isValidPhone = (phoneField, type, phoneLength) => {
     return typeof phoneField === type && phoneField?.trim()?.length === phoneLength
         ? phoneField?.trim()
         : false
 }
 
 // Check is password length is greater than 8 character
-utils._isValidPassword = (passwordField) => {
+validator._isValidPassword = (passwordField) => {
     return typeof passwordField === 'string' && passwordField?.length >= 8 ? passwordField : false
 }
 
